91d7bc4e0d8aa5a07bcb8c1ca22cf696dd45034c
Augie Winanda Favourite <fifoaugie06>
2021-02-27 14:06:47 +0700

    init

---
+++ core/AutoEncoder1.py
@@ -0,0 +1,46 @@
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Conv2DTranspose
+from tensorflow.keras.layers import LeakyReLU
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Reshape
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras import backend as K
+import numpy as np
+
+class ConvAutoEncoder:
+    @staticmethod
+    def build(width, height, depth, filters=(128,), latent_dim=48):
+        input_shape = (height, width, depth)
+        channel_dim = -1
+        inputs = Input(shape=input_shape)
+        x = inputs
+        # Encoder layer
+        for f in filters:
+            x = Conv2D(f, (3, 3), strides=2, padding='same')(x)
+            x = LeakyReLU(alpha=0.2)(x)
+            x = BatchNormalization(axis=channel_dim, name='enc_filter_' + str(f))(x)
+
+        volume_size = K.int_shape(x)
+        x = Flatten()(x)
+        # Latent layer
+        latent = Dense(latent_dim, name="encoded")(x)
+
+        # Decoder layer
+        x = Dense(np.prod(volume_size[1:]))(latent)
+        x = Reshape((volume_size[1], volume_size[2], volume_size[3]))(x)
+
+        # Reverse on decoder
+        for f in filters[::-1]:
+            x = Conv2DTranspose(f, (3, 3), strides=2, padding='same')(x)
+            x = LeakyReLU(alpha=0.2)(x)
+            x = BatchNormalization(axis=channel_dim, name='dec_filter_' + str(f))(x)
+
+        x = Conv2DTranspose(depth, (3, 3), padding="same")(x)
+        outputs = Activation("sigmoid", name="decoded")(x)
+
+        model = Model(inputs, outputs, name="auto_encoder")
+        return model
